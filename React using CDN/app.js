0c89216f6b396bfef98c468ca77e853e60a2e46e
Mahendra Pratap Das <mahendrapratapdas7@gmail.com>
2025-01-03 12:27:44 +0530

    Add the siblings and children in the React

--- React using CDN/app.js
+++ React using CDN/app.js
@@ -9,4 +9,20 @@ const ReactRoot= ReactDOM.createRoot(root)
 
 //Finally React Render the heading
 //render function convert the obj into the HTML but the Heading is the obj.
-ReactRoot.render(Heading)
+
+// ReactRoot.render(Heading)
+
+//Create the multiple children and siblings of a tag
+
+const container = React.createElement("div",{id:"parent"},
+    [React.createElement("h1", {id:"parentHeading"},"Parent Heading!"),
+        React.createElement("div",{id:"child"},
+            [React.createElement("h2",{id:"childHeading"}, "Child Heading"),
+                React.createElement("p",{id:"childParagraph"}, "Child Paragraph")
+
+            ]
+        )
+    ]
+)
+
+ReactRoot.render(container)
